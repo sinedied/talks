24c57e3ade3894c0787b89d4b9e9505415a7d43a
y.lasorsa <y.lasorsa@criteo.com>
2018-10-24 10:46:46 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express = require('express');
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello HT');
+});
+
+app.listen(3000, () => {
+  console.log('server is up!');
+});
