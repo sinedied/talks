7daf6fa8b448146129ffbf89ef7f265c3411a40a
sinedied <noda@free.fr>
2021-03-29 19:14:51 +0200

    Update prep script

--- github-actions/prepare.sh
+++ github-actions/prepare.sh
@@ -7,10 +7,12 @@ hub create gha-demo
 # Init new repo for GHA demo
 #npx create-react-app gha-demo
 cd gha-demo
+rm -rf .git
 git init
 git remote add origin git@github.com:sinedied/gha-demo.git
 git add .
 git commit -m "Initial commit"
-git push --set-upstream origin master
+git branch -M main
+git push --set-upstream origin main
 cd ..
 #rm -rf gha-demo
